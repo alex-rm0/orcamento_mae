76f74789f5e78a49a361c4c72e02fc11322f3d86
alex-rm0 <132898806+alex-rm0@users.noreply.github.com>
2024-08-18 11:22:49 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import gere_faturas_streamlit as gere_faturas
+import historico_faturas_streamlit as historico_faturas
+
+def main():
+    st.title("Aplicação Orçamento - Contas Mãe")
+    st.image("caminho_para_sua_imagem/foto_benedita.png", use_column_width=True)
+
+    menu = st.sidebar.selectbox(
+        "Escolha uma opção:",
+        ["Menu Principal", "Gerir Faturas", "Histórico de Faturas"]
+    )
+
+    if menu == "Menu Principal":
+        st.subheader("Bem-vindo ao Gerenciador de Orçamento")
+    elif menu == "Gerir Faturas":
+        gere_faturas.insert()
+    elif menu == "Histórico de Faturas":
+        historico_faturas.show()
+
+if __name__ == "__main__":
+    main()
